ddb27f7a309ab984200c5dbc149337ce2ac52dd3
Denis Stéphane <stph.denis@gmail.com>
2016-06-24 16:15:18 +0000

    mofification of config parameter to any

--- src/aurelia-meteor.js
+++ src/aurelia-meteor.js
@@ -2,7 +2,7 @@ import { BlazeAdapter } from './blaze-adapter';
 import { LoginButtons } from './login-buttons';
 import { Meteor } from './meteor';
 
-function configure(config: Mat<string, any>) { // eslint-disable-line no-unused-vars
+function configure(config: any) { // eslint-disable-line no-unused-vars
   config.globalResources(
     './login-buttons'
   );
