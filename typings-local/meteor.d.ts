bbe9577ac38685a3dd5e4836efd5728d0a57cb77
Denis Stéphane <stph.denis@gmail.com>
2016-08-10 10:16:43 +0000

    using typescript insteed of javascript

--- typings-local/meteor.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace DOM {
-  function injectStyles(str: string): void;
-}
-
-declare namespace FEATURE {
-  let shadowDOM: boolean;
-}
-
-declare function configureHtmlResourcePlugin(val: {globalResources: Function}): void;
