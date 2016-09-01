a68413f018358b56fa95a8280ad146c02f230d63
Denis Stéphane <stph.denis@gmail.com>
2016-09-01 08:35:42 +0000

    add import from accounts-ui

--- src/login-buttons.ts
+++ src/login-buttons.ts
@@ -2,6 +2,8 @@ import { bindable } from 'aurelia-framework';
 
 import { BlazeAdapter } from './blaze-adapter';
 
+import 'meteor/accounts-ui';
+
 export class LoginButtons extends BlazeAdapter {
   @bindable public align: string;
 
